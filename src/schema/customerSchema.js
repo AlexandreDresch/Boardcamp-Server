4be5fcd080b3e3b5ae519374353505e662f75440
z3r0-pc <z3r0-pc@pop-os.localdomain>
2023-02-09 23:51:13 -0300

    fix: POST customer cpf validation

--- src/schema/customerSchema.js
+++ src/schema/customerSchema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const customerSchema = Joi.object({
     name: Joi.string().required(),
     phone: Joi.string().required().min(10).max(11),
-    cpf: Joi.string().required().length(11),
+    cpf: Joi.string().required().length(11).regex(/^[0-9]+$/),
     birthday: Joi.date().raw().required(),
 });
